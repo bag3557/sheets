9bcb385209c803118dfadd4ffae311cd5877d567
ajitbhaik <ajit.bhaik@figmd.in>
2019-01-28 10:43:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line
+import React from 'react'
+import Table from './components/Table'
+
+const App = () =>
+  (<div style={{ width: 'max-content' }}>
+    <Table x={25} y={30} id={'1'} />
+  </div>)
+
+export default App
